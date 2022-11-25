12ac7f1bf3e845daf46b14d2ecd95b8d499a070f
Miggy <118282281+Bimbsicle@users.noreply.github.com>
2022-11-26 00:42:03 +0800

    Finish Exercise 4

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,14 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(array, num, num2, num3, num4) {
+    const result = array.filter(checkNum)
+    function checkNum(item) {
+        if (item === num || item === num2 || item === num3 || item === num4) {
+            return false
+        } 
+        else {
+            return item
+        }
+    }
+    return result
 };
 
 // Do not edit below this line
