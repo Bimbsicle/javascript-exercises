e73d9e0cb9774adddbd8d31d176ef5fad44be45f
Miggy <118282281+Bimbsicle@users.noreply.github.com>
2022-12-22 02:20:33 +0800

    Finish exercise

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,14 @@
-const fibonacci = function() {
-
+const fibonacci = function(a) {
+    if (a < 0) {
+        return "OOPS"
+    }
+    let sequence = [0,1]
+    let sum = 1
+    for (let i = 1; i < a; i++ ) {  
+      sum = sequence[i] + sequence[i-1]
+      sequence.push(sum)
+    }
+    return sum
 };
 
 // Do not edit below this line
