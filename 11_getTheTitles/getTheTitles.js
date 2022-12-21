5dcbf107678848f1281ddc8675836bad3cd7cb31
Miggy <118282281+Bimbsicle@users.noreply.github.com>
2022-12-22 02:36:31 +0800

    Finish exercise

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,5 +1,9 @@
-const getTheTitles = function() {
-
+const getTheTitles = function(a) {
+    let titles = []
+    for (let i = 0; i< a.length; i++) {
+        titles.push(a[i].title)     
+    }    
+    return titles
 };
 
 // Do not edit below this line
