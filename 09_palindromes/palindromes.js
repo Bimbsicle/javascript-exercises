3b55ebdb840d4a7e5b046bb8b7246fc6b0b18058
Miggy <118282281+Bimbsicle@users.noreply.github.com>
2022-12-22 00:08:38 +0800

    Finish Exercise

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,15 @@
-const palindromes = function () {
-
+const palindromes = function (a) {
+    let word = a.replace(/[^\w']|_/g, "")
+    let isPalindrome = true
+    for (let i = 0; i < word.length; i++) {
+      let testLetter1 = word[i] 
+      let testLetter2 = word[word.length - 1 - i] 
+      if (testLetter1.toUpperCase() !== testLetter2.toUpperCase() ) {
+        isPalindrome = false
+        break
+      }
+    }
+    return isPalindrome
 };
 
 // Do not edit below this line
