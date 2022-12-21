cc4076c2ddf6d1ec8d7c0f8d26a320ad6b459dd3
Miggy <118282281+Bimbsicle@users.noreply.github.com>
2022-12-22 03:01:03 +0800

    Finish Exercise

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,15 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(obj) {
+    for (let i = 0; i < obj.length; i++) {
+        if ('yearOfDeath' in obj[i] != true) {
+            obj[i].yearOfDeath = 2022
+        }  
+    }
+    let oldest = obj.sort(function(a,b) {
+        let firstPerson = a.yearOfDeath - a.yearOfBirth
+        let secondPerson = b.yearOfDeath - b.yearOfBirth
+        return firstPerson - secondPerson
+    })
+    return oldest[oldest.length-1]
 };
 
 // Do not edit below this line
